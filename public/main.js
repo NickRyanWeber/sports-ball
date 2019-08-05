4e164b1000238019d160de578bb5e0d389d69f42
nickryanweber <nicholasryanweber@gmail.com>
2019-08-05 13:36:37 -0400

    finished explorer mode

--- public/main.js
+++ public/main.js
@@ -5,3 +5,71 @@ const main = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+// My Stuff
+
+// Update Team One Name
+const teamOneNameUpdater = () => {
+  let teamOneName = document.querySelector('.team-1-input').value
+  console.log(teamOneName)
+  document.querySelector('.team-1-name').textContent = teamOneName
+}
+
+document
+  .querySelector('.update-team-1-name')
+  .addEventListener('click', teamOneNameUpdater)
+
+// Update Team Two name
+const teamTwoNameUpdater = () => {
+  let teamTwoName = document.querySelector('.team-2-input').value
+  console.log(teamTwoName)
+  document.querySelector('.team-2-name').textContent = teamTwoName
+}
+
+document
+  .querySelector('.update-team-2-name')
+  .addEventListener('click', teamTwoNameUpdater)
+
+// Team One Score Add
+const addScoreTeamOne = () => {
+  let teamOneScore = document.querySelector('.team-1-score').innerHTML
+  teamOneScore++
+  document.querySelector('.team-1-score').textContent = teamOneScore
+}
+
+document
+  .querySelector('.team-1-add-1-button')
+  .addEventListener('click', addScoreTeamOne)
+
+// Team One Score Subtract
+const subtractScoreTeamOne = () => {
+  let teamOneScore = document.querySelector('.team-1-score').innerHTML
+  teamOneScore--
+  document.querySelector('.team-1-score').textContent = teamOneScore
+}
+
+document
+  .querySelector('.team-1-subtract-1-button')
+  .addEventListener('click', subtractScoreTeamOne)
+
+// Team Two Score Add
+const addScoreTeamTwo = () => {
+  let teamTwoScore = document.querySelector('.team-2-score').innerHTML
+  teamTwoScore++
+  document.querySelector('.team-2-score').textContent = teamTwoScore
+}
+
+document
+  .querySelector('.team-2-add-1-button')
+  .addEventListener('click', addScoreTeamTwo)
+
+// Team Two Score Subtract
+const subtractScoreTeamTwo = () => {
+  let teamTwoScore = document.querySelector('.team-2-score').innerHTML
+  teamTwoScore--
+  document.querySelector('.team-2-score').textContent = teamTwoScore
+}
+
+document
+  .querySelector('.team-2-subtract-1-button')
+  .addEventListener('click', subtractScoreTeamTwo)
